3697c5cccc165bf7d4bae3c6b606fda7bb180fcb
Ahmed Dahmani <dahmani@brainstack.net>
2025-07-15 15:55:53 +0100

    Added building blocks

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, string Description, List<string> Category, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException(); 
+    }
+}
+
+
