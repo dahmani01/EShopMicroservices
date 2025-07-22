973c60044660dee81eb7a4bcb51118168140151d
dahmani01 <adahmani010@gmail.com>
2025-07-22 17:56:19 +0100

    feat: added getproductbyId handler and endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>; 
+
+public record GetProductByIdResult(Product Product);
+
+internal class GetProductByIdHandler(IDocumentSession session, ILogger<GetProductByIdResult> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductById query called {@Query}", query);
+
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if (product == null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByIdResult(product); 
+    }
+}
