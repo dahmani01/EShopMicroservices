69f7bdffc7627f2dd7a44a2dbddf6242b69ef05b
dahmani01 <adahmani010@gmail.com>
2025-07-28 18:59:13 +0100

    feat: Basket.API intial setup

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -1,5 +1,25 @@
 ﻿namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketEndpoint
+public record GetBasketRequest(string UserName);
+
+public record GetBasketResponse(ShoppingCart ShoppingCart);
+
+public class GetBasketEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.Map("/basket/{userName}", async(string userName, ISender sender) =>
+        {
+            var result = await sender.Send(new GetBasketQuery(userName)); 
+
+            var response = result.Adapt<GetBasketResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetBasketQuery")
+        .Produces<GetBasketResponse>()
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Basket by UserName")
+        .WithDescription("Get Basket by UserName"); 
+    }
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,12 +1,51 @@
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+using Carter;
+using FluentValidation;
+using Marten;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
-//Add Services to the container
+//Add services to the container.
+var assembly = typeof(Program).Assembly;
 
+builder.Services.AddMediatR((config) =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
+});
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddCarter();
+
+builder.Services.AddMarten(options => { options.Connection(builder.Configuration.GetConnectionString("Database")!); })
+    .UseLightweightSessions();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+}
+
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database"));
 
 var app = builder.Build();
 
-//Configure HTTP Pipeline
+// Configure the HTTP request pipeline.
+app.MapCarter();
 
-app.MapGet("/", () => "Hello World!");
+app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
