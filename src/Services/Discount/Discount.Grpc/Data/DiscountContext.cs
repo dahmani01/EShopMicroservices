835a014d99206501d558cbec5f2e240a1510442c
Ahmed Dahmani <ahmeddahmani@issatso.u-sousse.tn>
2025-12-14 17:00:34 +0100

    feat: Configure SQLite database with initial migration and seed data for Coupons

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -6,4 +6,11 @@ namespace Discount.Grpc.Data;
 public class DiscountContext(DbContextOptions<DiscountContext> options) : DbContext(options)
 {
     DbSet<Coupon> Coupons { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { Id = 1, ProductName = "Honor x6c", Description = "Youtube Discount", Amount = 150 },
+            new Coupon { Id = 2, ProductName = "Pixel 9a", Description = "Youtube Discount", Amount = 100 });
+    }
 }
